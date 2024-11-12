59e2a8110c1d18374e2e0e9607516b14d0a6d5c7
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2024-11-12 02:00:27 -0300

    Add repetition structures with do-while and while loops in env.js

--- class15/env.js
+++ class15/env.js
@@ -1,5 +1,16 @@
 var c = 1
+
+// repetition structure with logical test at the end
+do {
+    console.log('second test: ', c)
+    c++
+}  while(c <= 10)
+
+// repetition structure with logical test at the beginning
+/*
 while(c <= 10) {
-    console.log(c)
+    console.log('first test: ', c)
     c++
 }
+*/
+
